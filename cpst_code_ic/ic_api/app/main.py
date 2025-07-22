eaf5b6e1b7f816f77263e4f5f26e446e9e8d27ec
Sindhu S <51540427+simanyas@users.noreply.github.com>
2025-07-22 10:48:23 +0530

    Update main.py

--- cpst_code_ic/ic_api/app/main.py
+++ cpst_code_ic/ic_api/app/main.py
@@ -11,7 +11,7 @@ from fastapi import APIRouter, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 import tensorflow as tf
-from app.api import api_router, run_rlmf
+from app.api import api_router, run_rlmf, done_with_rl_mf
 from app.config import settings
 import uvicorn
 
@@ -51,25 +51,32 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-input_img = gr.Image(label="Input Picture")
-output_img = gr.Image(label="Image")
-caption_us = gr.Textbox(label="Model under evaluation")
-yolo_res = gr.Textbox(label="Yolo result")
-yolo_gemini_caption = gr.Textbox(label="Caption suggestion from expert model")
-#done_with_rl_mf_btn = gr.Button()
+css = """
+  #output {
+    height: 500px;
+    overflow: auto;
+    border: 1px solid #ccc;
+  }
+"""
 
-# Create Gradio interface object
-iface = gr.Interface(fn = run_rlmf,
-                         inputs = [input_img],
-                         outputs = [output_img, caption_us, yolo_res, yolo_gemini_caption],
+with gr.Blocks(css=css) as demo:
+    gr.Markdown("RLMF With Expert Model")
+    with gr.Tab(label="RLMF"):
+        output_img = gr.Image(label="Image")
+        caption_us = gr.Textbox(label="Model under evaluation")
+        yolo_res = gr.Textbox(label="Yolo result")
+        yolo_gemini_caption = gr.Textbox(label="Caption suggestion from expert model")
+        done_with_rl_mf_btn = gr.Button(value="Submit RL Data For Finetuning Model")
+        run_rl_mf_btn = gr.Button(value="Run RLMF")
+
+run_rl_mf_btn.click(run_rlmf,inputs=[],outputs=[output_img, caption_us, yolo_res, yolo_gemini_caption],
                          title="Real or Kidding",
                          description="YOLO/Gemini Feedback",
-                         allow_flagging='never',
-                         )
-#done_with_rl_mf_btn.onClick()
+                         allow_flagging='never')
+done_with_rl_mf_btn.click(done_with_rl_mf)
 
 # Mount gradio interface object on FastAPI app at endpoint = '/'
-app = gr.mount_gradio_app(app, iface, path="/")
+app = gr.mount_gradio_app(app, demo, path="/")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001) 
